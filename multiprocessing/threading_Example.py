4fc89bc9af23981cdcb65bb8f9d732229f4e30d6
Krishna <studyroom19471@gmail.com>
2024-01-29 14:12:40 +0530

    Implementing multiprocessing by calling other funtion without waiting to complete it

---
+++ multiprocessing/threading_Example.py
@@ -0,0 +1,29 @@
+import threading
+import time
+
+# Define the first function to run in a separate thread
+def function_one():
+    time.sleep(5)
+    print("Function One is running after 5 second.")
+    pass
+
+# Define the second function to run in a separate thread
+def function_two():
+    time.sleep(3)
+    print("Function Two is running after 3 second.")
+    pass
+
+# Create two threads for each function
+thread_one = threading.Thread(target=function_one)
+thread_two = threading.Thread(target=function_two)
+
+# Start both threads
+print("I am calling one fuction then calling other without waiting to complete first one")
+thread_one.start()
+print("Somethig in between")
+thread_two.start()
+print("Calling funvtioin is complete")
+
+# Wait for both threads to finish (this won't happen in this example)
+# thread_one.join()
+# thread_two.join()
